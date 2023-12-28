3a2f2d52d70577371124c8c8997113aa51c1b988
Rishabh Soni <rs898@kent.ac.uk>
2023-12-28 12:29:27 +0000

    Update file LSTM.py

--- Implementation/LSTM.py
+++ Implementation/LSTM.py
@@ -1,18 +1,22 @@
 import numpy as np
 import yfinance as yf
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+from keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
 import pandas as pd
 
-def fetch_data(symbol, start_date="2020-01-01", end_date="2023-01-01"):
+# Function to fetch stock data
+def fetch_data(symbol, start_date="2015-01-01", end_date="2023-01-01"):
     stock_data = yf.download(symbol, start=start_date, end=end_date)
     stock_data = stock_data.ffill()
     stock_data['EMA'] = stock_data['Close'].ewm(span=12, adjust=False).mean()
     return stock_data
 
-def preprocess_data(stock_data, sequence_length=10):
+# Function to preprocess data
+def preprocess_data(stock_data, sequence_length=25):
     scaler = MinMaxScaler()
     scaled_data = scaler.fit_transform(stock_data["EMA"].values.reshape(-1, 1))
     train_size = int(len(scaled_data) * 0.8)
@@ -28,6 +32,7 @@ def preprocess_data(stock_data, sequence_length=10):
 
     return X_train, X_test, y_train, y_test, scaler
 
+# Function to create sequences
 def create_sequences(data, sequence_length):
     sequences = []
     for i in range(len(data) - sequence_length):
@@ -35,52 +40,69 @@ def create_sequences(data, sequence_length):
         sequences.append(seq)
     return np.array(sequences)
 
+# Function to build LSTM model
 def build_model(sequence_length):
     model = Sequential()
     model.add(LSTM(150, activation="relu", input_shape=(sequence_length, 1)))
-    # model.add(Dropout(0.2))  # dropout layer to prevent overfitting
     model.add(Dense(1))
     model.compile(optimizer="RMSprop", loss="mean_squared_error")
     return model
 
-def train_model(model, X_train, y_train, epochs=150, batch_size=64):
+# Function to train the model
+def train_model(model, X_train, y_train, epochs=100, batch_size=32):
     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
 
+# Function to predict data
 def predict_data(model, X_test):
     return model.predict(X_test)
 
+# Function to inverse transform scaled data
 def inverse_transform(scaler, data):
     return scaler.inverse_transform(data)
 
-def plot_predictions(stock_data, date_range, y_test_actual, y_pred_actual, symbol):
-    plt.figure(figsize=(12, 6))
-    plt.plot(stock_data.index, stock_data['EMA'], label="Actual dataset (Entire Dataset)", color="gray", linewidth=1)
-    plt.plot(date_range, y_test_actual, label="Actual (Test Data)", linewidth=1)
-    plt.plot(date_range, y_pred_actual, label="Predicted EMA", linewidth=1)
+# Initialize Dash app
+app = dash.Dash(__name__)
 
-    plt.text(date_range[-1] + pd.DateOffset(days=3), y_test_actual[-1][0] + 2,
-             f'Actual: {y_test_actual[-1][0]:.2f}', ha='left', va='center', fontsize=10)
+# Define app layout
+app.layout = html.Div([
+    html.H1("Stock Price Prediction with LSTM"),
+    dcc.Input(id='symbol-input', type='text', value='AAPL', placeholder='Enter a stock symbol'),
+    dcc.Graph(id='prediction-plot')
+])
 
-    plt.text(date_range[-1] + pd.DateOffset(days=3), y_pred_actual[-1][0] - 2,
-             f'Predicted: {y_pred_actual[-1][0]:.2f}', ha='right', va='center', fontsize=10)
-
-    plt.xlabel("Date")
-    plt.ylabel("Stock Price")
-    plt.title(f"{symbol} Stock Price Prediction")
-    plt.legend()
-    plt.show()
-
-def run(symbol, start_date="2020-01-01", end_date="2023-01-01", sequence_length=10, epochs=100, batch_size=32):
-    stock_data = fetch_data(symbol, start_date, end_date)
-    X_train, X_test, y_train, y_test, scaler = preprocess_data(stock_data, sequence_length)
-    model = build_model(sequence_length)
-    train_model(model, X_train, y_train, epochs, batch_size)
+# Update the graph based on user input
+@app.callback(
+    Output('prediction-plot', 'figure'),
+    [Input('symbol-input', 'value')]
+)
+def update_plot(symbol_input):
+    symbol = symbol_input.upper()
+    stock_data = fetch_data(symbol)
+    X_train, X_test, y_train, y_test, scaler = preprocess_data(stock_data)
+    model = build_model(sequence_length=25)
+    train_model(model, X_train, y_train)
     y_pred = predict_data(model, X_test)
     y_test_actual = inverse_transform(scaler, y_test)
     y_pred_actual = inverse_transform(scaler, y_pred)
     date_range = stock_data.index[-len(y_test):]
-    plot_predictions(stock_data, date_range, y_test_actual, y_pred_actual, symbol)
 
-# usage:
-symbol_input = input("Enter a stock symbol: ")
-run(symbol_input)
+    # Plotly graph for Dash
+    figure = {
+        'data': [
+            {'x': stock_data.index, 'y': stock_data['EMA'], 'type': 'line', 'name': 'Actual dataset (Entire Dataset)'},
+            {'x': date_range, 'y': y_test_actual.flatten(), 'type': 'line', 'name': 'Actual (Test Data)'},
+            {'x': date_range, 'y': y_pred_actual.flatten(), 'type': 'line', 'name': 'Predicted EMA'},
+        ],
+        'layout': {
+            'xaxis': {'title': 'Date'},
+            'yaxis': {'title': 'Stock Price'},
+            'title': f"{symbol} Stock Price Prediction",
+            'legend': {'x': 0, 'y': 1}
+        }
+    }
+
+    return figure
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
