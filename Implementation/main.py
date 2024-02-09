e7c1021c2509bed56c532c6969080ee9404a58f6
Rishabh Soni <rs898@kent.ac.uk>
2024-02-09 19:53:02 +0000

    ChatGPT implementation, ChatBox and Formatting. Authored-by: Steven Li <sl746@kent.ac.uk> Implemented OpenAI API + Functions Modified exisiting elements and callbacks to improve functionality Fixed UI element placement Improved comments Added gridding for rt-stock Used GPT to summarise stock desc
    Co-authored-by: Rishabh Soni <rs898@kent.ac.uk>
    Improved UI Colours
    Impletmented ChatBox
    Promting for OpenAI
    Assisted in implementing OpenAI API + Functions
    Assisted in adding grids for rt-stock

--- Implementation/main.py
+++ Implementation/main.py
@@ -9,7 +9,19 @@ from sklearn.preprocessing import MinMaxScaler
 import io
 import base64
 import matplotlib.pyplot as plt
-import openai
+import os
+from openai import OpenAI
+
+# Colour palette 463f3a-8a817c-bcb8b1-f4f3ee-e0afa0
+primary1 = "#bcb8b1"
+primary2 = "#8a817c"
+primary3 = "#463f3a"
+secondary1 = "#f4f3ee"
+secondary2 = "#e0afa0"
+
+# Set configs
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
 
 # Function to fetch stock data
 def fetch_data(symbol, start_date="2015-01-01", end_date="2023-01-01"):
@@ -18,11 +30,13 @@ def fetch_data(symbol, start_date="2015-01-01", end_date="2023-01-01"):
     stock_data["EMA"] = stock_data["Close"].ewm(span=12, adjust=False).mean()
     return stock_data
 
+
 # Function to fetch company data
 def fetch_company_data(symbol):
     ticker = yf.Ticker(symbol)
     info = ticker.info
     return info
+
 
 # Function to preprocess data
 def preprocess_data(stock_data, sequence_length=25):
@@ -41,6 +55,7 @@ def preprocess_data(stock_data, sequence_length=25):
 
     return X_train, X_test, y_train, y_test, scaler
 
+
 # Function to create sequences
 def create_sequences(data, sequence_length):
     sequences = []
@@ -48,6 +63,7 @@ def create_sequences(data, sequence_length):
         seq = data[i:i + sequence_length]
         sequences.append(seq)
     return np.array(sequences)
+
 
 # Function to build LSTM model
 def build_model(sequence_length):
@@ -57,17 +73,21 @@ def build_model(sequence_length):
     model.compile(optimizer="RMSprop", loss="mean_squared_error")
     return model
 
+
 # Function to train the model
 def train_model(model, X_train, y_train, epochs=100, batch_size=32):
     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+
 
 # Function to predict data
 def predict_data(model, X_test):
     return model.predict(X_test)
 
+
 # Function to inverse transform scaled data
 def inverse_transform(scaler, data):
     return scaler.inverse_transform(data)
+
 
 # Function to generate stock information
 def get_stock_info(symbol):
@@ -76,6 +96,22 @@ def get_stock_info(symbol):
         return stock_info
     except:
         return None
+
+
+# Function make a call to the OpenAI API to generate a response
+# "You are an information board providing stock information to the users."
+def generate_response(request):
+    context = "You are a stock analyst assistant who provides stock information to the user."
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature=0.4,
+        messages=[
+            {"role": "system", "content": context},
+            {"role": "user", "content": request}
+        ],
+    )
+    return completion.choices[0].message.content
+
 
 # External CSS styles
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -86,16 +122,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # Define app layout with improved styling
 app.layout = html.Div(
     [
+        # Header
         html.H1(
             "Stock Price Prediction",
-            style={"textAlign": "center", "color": "#463f3a"},
+            style={"textAlign": "center", "color": primary3, "font-weight": "bold"},
         ),
         html.H3(
             "Long Short Term Memory",
-            style={"textAlign": "center", "color": "#463f3a", "marginBottom": "20px"},
+            style={"textAlign": "center", "color": primary3, "marginBottom": "10px"},
         ),
+
+        # Main divider for user area
         html.Div(
             [
+                # User inputs for analysis
                 dcc.Input(
                     id="symbol-input",
                     type="text",
@@ -144,35 +184,36 @@ app.layout = html.Div(
                     style={
                         "margin": "auto",
                         "borderRadius": "5px",
-                        "background-color": "#bcb8b1",
+                        "background": primary3,
                         "textAlign": "center",
-                        "color": "white",
+                        "color": secondary1,
                         "border": "auto",
-                        "font-size": "18px",
+                        "font-size": "15px",
                         "display": "inline-block",
                     },
                 ),
 
-                # Loading bar whilst the program is running on the terminal
+                # Display information about the company to the user
+                dcc.Loading(
+                    type="circle",
+                    children=[html.Div(id="company-info", style={"marginBottom": "20px", "min-height": "50px"})],
+                ),
+
+                # Display the prediction graph to the user
                 dcc.Loading(
                     id="loading-output",
                     type="circle",
                     children=[
-                        html.Div(id="company-info", style={"marginBottom": "20px"}),
                         dcc.Graph(id="prediction-plot", style={"paddingBottom": "10px"}),
                         html.A(
-                            html.Button("Download Graph"),
+                            html.Button("Download Graph", style={
+                                "borderRadius": "5px",
+                                "background": primary3,
+                                "color": secondary1}, ),
                             id="download-link",
                             download="prediction_graph.png",
                             href="",
                             target="_blank",
-                            style={
-                                "marginLeft": "20px",
-                                "padding": "10px",
-                                "borderRadius": "5px",
-                                "background-color": "#bcb8b1",
-                                "border": "auto",
-                            },
                         ),
                     ],
                 ),
@@ -180,46 +221,107 @@ app.layout = html.Div(
 
             style={"width": "80%", "margin": "auto", "textAlign": "center"},
         ),
-        html.Div(id="stock-info", style={"textAlign": "center", "color": "#463f3a", "fontWeight": "bold"}),
-        dcc.Graph(id="loss-plot",
-                  style={"width": "80%", "margin": "auto", "paddingBottom": "20px", "paddingTop": "10px"}),
+
+        # Chat Interface
+        html.Div([
+            html.Div(id="chat-container", children=[
+                html.Div(children=[
+                    html.Span(children="Welcome to the Stock Information Chat!", style={"font-weight": "bold"})
+                ]),
+            ], style={"background": primary2, "color": secondary1, "borderRadius": "5px",
+                      "padding": "10px", "margin": "10px 0"}),
+            dcc.Loading(
+                type="circle",
+                children=[
+                    html.Div(id="chat-output-text",
+                             style={"background": "#e6e6e6",
+                                    "borderRadius": "5px",
+                                    "padding": "10px",
+                                    "margin": "10px 0",
+                                    "height": "300px",
+                                    "overflowY": "scroll"}), ]
+            ),
+            html.Div(children=[
+                dcc.Textarea(id="chat-input-text", value="", placeholder="Type your message...",
+                             style={"width": "100%", "height": "100px"}),
+                html.Button("Submit", id="chat-submit-button",
+                            style={"width": "95%",
+                                   "background": primary3,
+                                   "color": secondary1,
+                                   "margin": "auto",
+                                   "border": "none",
+                                   "cursor": "pointer",
+                                   "textAlign": "center"})
+            ])
+        ],
+            style={"width": "80%", "margin": "auto", "marginTop": "20px", "paddingBottom": "10px",
+                   "background": primary1, "borderRadius": "15px", "textAlign": "center"}
+        ),
+
+        # Display extra information for user
+        html.Div(id="rt-stock-info", style={"paddingTop": "20px"}),
+        html.Div(
+            id="stock-info",
+            children=[],
+            style={"width": "80%", "display": "grid", "grid-template-columns": "repeat(2, 1fr)", "gap": "5px",
+                   "textAlign": "center", "margin": "auto", "paddingBottom": "20px", "paddingTop": "5px"},
+        ),
+        dcc.Loading(
+            type="circle",
+            children=[
+                dcc.Graph(id="loss-plot",
+                          style={"width": "80%", "margin": "auto", "paddingBottom": "10px", "paddingTop": "10px"})]
+        ),
     ],
     style={
         "width": "100%",
-        "background-color": "#f4f3ee"
+        "background": secondary1
     },
 )
-# Set your OpenAI API key
-openai.api_key = "your_openai_api_key"
-
-# Function to ask questions to ChatGPT
-def ask_chatgpt(question):
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # Choose an appropriate engine
-            prompt=f"Ask GPT: {question}",
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-        return response.choices[0].text.strip()
-    except Exception as e:
-        return f"Error in asking ChatGPT: {str(e)}"
 
 
+# Update company information
+@app.callback(
+    [
+        Output("company-info", "children"),
+        Output("chat-input-text", "value")
+    ],
+    [Input("search-button", "n_clicks")],
+    [
+        State("symbol-input", "value")
+    ]
+)
+def update_company(n_clicks, symbol_input):
+    if n_clicks > 0:
+        # Fetch information about the company
+        company_data = fetch_company_data(symbol_input)
 
+        # Summarise long summary
+        summary = generate_response("Summarise in a short paragraph: " + company_data["longBusinessSummary"])
 
+        # Create element to hold information
+        company_info = [html.P([html.H2(company_data["longName"]),
+                                html.P(summary)])]
+
+        # Prompt for stock symbol
+        promt = ("Evaluate important events that caused the stock price of " + company_data["longName"]
+                 + " (" + symbol_input + ") to change.")
+
+        # Return updates
+        if company_info:
+            return company_info, promt
+    else:
+        return dash.no_update, dash.no_update
 
 
 # Update the company info, plot, and real-time stock info based on user input
 @app.callback(
     [
-        Output("company-info", "children"),
-        Output("stock-info", "children"),
         Output("prediction-plot", "figure"),
         Output("download-link", "href"),
-        Output("loss-plot", "figure")
+        Output("rt-stock-info", "children"),
+        Output("stock-info", "children"),
+        Output("loss-plot", "figure"),
     ],
     [Input("search-button", "n_clicks")],
     [
@@ -229,27 +331,23 @@ def ask_chatgpt(question):
         State("epoch-input", "value")
     ]
 )
-def update_company_and_plot(n_clicks, symbol_input, start_date, end_date, epochs):
+def update_plot_and_info(n_clicks, symbol_input, start_date, end_date, epochs):
     if n_clicks > 0:
         symbol = symbol_input.upper()
         stock_data = fetch_data(symbol, start_date, end_date)
         X_train, X_test, y_train, y_test, scaler = preprocess_data(stock_data)
         model = build_model(sequence_length=25)
 
-     # Ask a question to ChatGPT
-        chatgpt_question = f"What can you tell me about {symbol_input} stocks?"
-        chatgpt_response = ask_chatgpt(chatgpt_question) 
-
-     # Train the model and get training history
+        # Train the model and get training history
         history = model.fit(X_train, y_train, epochs=epochs, batch_size=32)
 
-     # Get the predicted data and actual data
+        # Get the predicted data and actual data
         y_pred = predict_data(model, X_test)
         y_test_actual = inverse_transform(scaler, y_test)
         y_pred_actual = inverse_transform(scaler, y_pred)
         date_range = stock_data.index[-len(y_test):]
 
-     # Plotly graph for Dash
+        # Plotly graph for Dash
         figure = {
             "data": [
                 {"x": stock_data.index, "y": stock_data["EMA"], "type": "line", "name": "Dataset"},
@@ -264,12 +362,12 @@ def update_company_and_plot(n_clicks, symbol_input, start_date, end_date, epochs
             }
         }
 
-    # Plotly graph for loss
+        # Plotly graph for loss
         loss_figure = {
             "data": [
                 {
                     "x": list(range(1, epochs + 1)),
-                    "y": history.history['loss'],
+                    "y": history.history["loss"],
                     "type": "line",
                     "name": "Training Loss"
                 },
@@ -282,7 +380,7 @@ def update_company_and_plot(n_clicks, symbol_input, start_date, end_date, epochs
             }
         }
 
-     # Update download link
+        # Update download link
         img_data = io.BytesIO()
         plt.figure(figsize=(8, 6))
         plt.plot(stock_data.index, stock_data["EMA"], label="Dataset")
@@ -297,30 +395,44 @@ def update_company_and_plot(n_clicks, symbol_input, start_date, end_date, epochs
         base64_img = base64.b64encode(img_data.read()).decode("utf-8")
         download_href = f"data:image/png;base64,{base64_img}"
 
-    # Update both outputs
-        company_data = fetch_company_data(symbol)
-        company_info = [html.P([html.H2(company_data["longName"]),
-                                html.P(company_data["longBusinessSummary"])])]
-
-    # Get real-time stock information
+        # Get real-time stock information
         stock_info = get_stock_info(symbol)
+        current_stock = html.Div(f"Real-time Stock Price of {stock_info['longName']}",
+                                 style={"paddingTop": "5px", "paddingBottom": "5px", "background": primary2,
+                                        "color": secondary1, "borderRadius": "5px", "textAlign": "center",
+                                        "width": "80%", "margin": "auto", "font-weight": "bold"})
+
         stock_info_display = []
         if stock_info:
-            stock_info_display.append(html.P(f"Real-time Stock Price of {stock_info['longName']}"))
-            stock_info_display.append(html.P(f"Symbol: {stock_info['symbol']}"))
-            stock_info_display.append(html.P(f"Current Price: {stock_info['ask']}"))
-            stock_info_display.append(html.P(f"Open: {stock_info['open']}"))
-            stock_info_display.append(html.P(f"High: {stock_info['dayHigh']}"))
-            stock_info_display.append(html.P(f"Low: {stock_info['dayLow']}"))
+            styles = {"padding": "10px", "background": primary1, "borderRadius": "5px"}
+            stock_info_display = [
+                html.Div(f"Current Price: {stock_info['ask']}", style=styles),
+                html.Div(f"Open: {stock_info['open']}", style=styles),
+                html.Div(f"Low: {stock_info['dayLow']}", style=styles),
+                html.Div(f"High: {stock_info['dayHigh']}", style=styles),
+            ]
 
-        return company_info, stock_info_display, figure, download_href, loss_figure, html.P(f"ChatGPT Response: {chatgpt_response}"),  # Display ChatGPT response 
+        # Return updates
+        return figure, download_href, current_stock, stock_info_display, loss_figure
     else:
-    # Return no update for all outputs if no clicks
         return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+
+
+# Used for ChatGPT ChatBox
+@app.callback(
+    Output("chat-output-text", "children"),
+    [Input("chat-submit-button", "n_clicks")],
+    [State("chat-input-text", "value")]
+)
+def user_chat(n_clicks, input_text):
+    if n_clicks is not None:
+        # Send user input to function to call API
+        output = generate_response(input_text)
+        return output
+
 
 # Run the app
 if __name__ == "__main__":
     app.run_server(debug=True)
 
 # colour palette 463f3a-8a817c-bcb8b1-f4f3ee-e0afa0
-
